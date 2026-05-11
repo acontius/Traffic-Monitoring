396b4f891e33beb707673059b812d470e5497b18
acontius <ahuram188@gmail.com>
2026-05-12 01:25:13 +0330

    added the API service for testing the datas in the database

---
+++ API/api.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI
+import asyncpg
+import json
+
+DB_DSN = "postgresql://acontius:1234@localhost:5432/traffic_monitoring"
+
+app = FastAPI()
+pool: asyncpg.pool.Pool = None
+
+
+@app.on_event("startup")
+async def startup():
+    global pool
+    pool = await asyncpg.create_pool(DB_DSN)
+    print("API connected to database.")
+
+
+@app.get("/latest")
+async def get_latest_all():
+    """
+    آخرین داده برای هر device
+    """
+    query = """
+        SELECT DISTINCT ON (device_id)
+        device_id, timestamp, payload
+        FROM traffic_data
+        ORDER BY device_id, timestamp DESC
+    """
+    async with pool.acquire() as con:
+        rows = await con.fetch(query)
+
+    return [
+        {
+            "device_id": r["device_id"],
+            "timestamp": r["timestamp"],
+            "payload": r["payload"],
+        }
+        for r in rows
+    ]
+
+
+@app.get("/device/{device_id}")
+async def get_latest_device(device_id: str):
+    """
+    آخرین داده مربوط به یک device
+    """
+    query = """
+        SELECT device_id, timestamp, payload
+        FROM traffic_data
+        WHERE device_id = $1
+        ORDER BY timestamp DESC
+        LIMIT 1
+    """
+    async with pool.acquire() as con:
+        row = await con.fetchrow(query, device_id)
+
+    if not row:
+        return {"error": "device not found"}
+
+    return {
+        "device_id": row["device_id"],
+        "timestamp": row["timestamp"],
+        "payload": row["payload"],
+    }
